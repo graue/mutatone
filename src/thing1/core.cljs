a84b46139c2f40f891a1bd4ed7a93363799d7461
Scott Feeney <scott@oceanbase.org>
2013-12-07 16:31:55 -0500

    Milestone: music plays!

--- src/thing1/core.cljs
+++ src/thing1/core.cljs
@@ -99,6 +99,66 @@
         chromatic-intervals (dia->chrom intervals scale)]
     (map (partial + octave-offset note-offset) chromatic-intervals)))
 
-(. js/console (log "Hello world!"))
+(defn play-note-with-adsr [osc gain-node freq amp time length a d s r]
+  ;; Yes, this has way too many parameters. :|
+  (.setValueAtTime (.-frequency osc) freq time)
+  (.setValueAtTime (.-gain gain-node) 0 time)
+  (.linearRampToValueAtTime (.-gain gain-node) 1 (+ time a))
+  (.linearRampToValueAtTime (.-gain gain-node) s (+ time a d))
+  (.setValueAtTime (.-gain gain-node) s (+ time length (- r)))
+  (.linearRampToValueAtTime (.-gain gain-node) 0 (+ time length)))
+
+(defn play-notes
+  [osc gain-node notes length]
+  (let [ctx (.-context osc)
+        current-time-fixme (hum/curr-time ctx)
+        start (+ 0.15 (hum/curr-time ctx))]
+    (dotimes [i (count notes)]
+      (let [note (nth notes i)
+            start (+ start (* i length))]
+
+        (when note  ; Ignore nil note values, treating them as rests.
+          (play-note-with-adsr osc gain-node (midi->hz note) 1.0
+                               start length 0.05 0.05 0.7 0.0625))))))
+
+(def ctx (atom nil))
+(def osc (atom nil))
+(def filt (atom nil))
+(def gain (atom nil))
+
+(defn create-osc-and-stuff []
+  (reset! ctx (hum/create-context))
+  (reset! osc (hum/create-osc @ctx :triangle))
+  (set! (.-type @osc) "triangle")  ; Otherwise we get a sine in Firefox...?
+  (reset! filt (hum/create-biquad-filter @ctx))
+  (reset! gain (hum/create-gain @ctx))
+  (hum/connect @osc @filt)
+  (hum/connect @filt @gain)
+  (hum/start-osc @ctx @osc)
+  (hum/connect-output @ctx @gain)
+  nil)
+
+(defn panic
+  "Kill all notes."
+  []
+  (let [time (hum/curr-time (.-context @gain))]
+    (.cancelScheduledValues (.-frequency @osc) time)
+    (.cancelScheduledValues (.-gain @gain) time)
+    (.setValueAtTime (.-gain @gain) 0 time)))
 
 (repl/connect "http://localhost:9000/repl")
+
+(comment
+  (play-notes @osc @gain
+              (scalify [0
+                        0 3
+                        0 3 1
+                        0 3 1 -1
+                        0 3 1 -1 -2
+                          3 1 -1 -2
+                            1 -1 -2
+                              -1 -2
+                                 -2]
+                       "major pentatonic" "d#" 6)
+              0.75)
+  )
