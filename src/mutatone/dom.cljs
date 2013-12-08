0d6466834d60ebc86b6f2fa23b0aa1502a6b31a4
Scott Feeney <scott@oceanbase.org>
2013-12-07 19:10:05 -0500

    Print melodies and buttons

--- src/mutatone/dom.cljs
+++ src/mutatone/dom.cljs
@@ -1,8 +1,24 @@
 (ns mutatone.dom
   (:require [dommy.utils :as utils]
-            [dommy.core :as dommy])
-  (:require-macros [dommy.macros :refer [node sel sel1]]))
+            [dommy.core :as dommy]
+            [mutatone.theory :refer [phrase->str scalify]])
+  (:require-macros [dommy.macros :refer [node sel sel1 deftemplate]]))
+
+(deftemplate melody-template [melody idx]
+  [:span
+    [:button {:class "play-btn" :data-idx idx} "Play"]
+    [:button {:class "breed-btn" :data-idx idx} "Breed"]
+    (phrase->str (scalify (:intervals melody)
+                          (:scale melody)
+                          (:root melody)))])
+
+(deftemplate main-ui [melodies]
+  [:div
+    [:ul
+      (for [i (range (count melodies))]
+        [:li (melody-template (nth melodies i) i)])
+    [:p "Then some more stuff"]]])
 
 (defn render-melodies [melodies]
   (dommy/replace! (sel1 :#melodies)
-                   (node [p "This is a thing, cookies!!"])))
+                  (main-ui melodies)))
