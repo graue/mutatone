d2e544abcbe115173a56baffa0819c5aa65cb321
Scott Feeney <scott@oceanbase.org>
2013-12-23 03:30:44 -0500

    Update deps to prepare for om work

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
             :url "https://github.com/graue/mutatone/blob/master/MIT-LICENSE.txt"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2080"]
+                 [org.clojure/clojurescript "0.0-2127"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [hum "0.2.4"]
+                 [om "0.1.0-SNAPSHOT"]
+                 [sablono "0.1.1"]
                  [prismatic/dommy "0.1.2"]]
 
   :plugins [[lein-cljsbuild "1.0.0"]]
